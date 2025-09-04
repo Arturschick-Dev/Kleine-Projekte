1e48ec709aded5b541ad613431ee4ea756b94f52
Artur Krasikov <arturlein@googlemail.com>
2025-09-04 09:13:28 +0200

    Erster Einsteig

---
+++ zahlen_raten_spiel_v1.py
@@ -0,0 +1,31 @@
+# V1
+# Made by Arturschick
+# 
+
+import random
+spielen = True
+
+def spiel_Start():
+    global spielen
+    zahl = random.randint(1,100)
+    while spielen is True:
+        print("Eine Zahl wurde bereits für dich festgelegt")
+        print("Randomzahl:",zahl)
+        eingabe = int(input("Welche Zahl wählst du?\nZahl: "))
+        if eingabe in range(0, 100):
+            if eingabe == zahl:
+                print("Du hast richtig geraten! Woho!!!\nMöchtest du weiterspielen?\n\nWeiterspielen = y/Y")
+                weiterspielen = input("\n Eingabe: ")
+                if weiterspielen == "y":
+                    spiel_Start()
+                else:
+                    spielen = False
+                    break
+                print("Ungültige Eingabe, Nutze nur Zahlen zwischen 1 und 100")
+
+
+print("Willkommen bei 'Errate die Zahl'\nLass uns gleich anfangen!")
+print("\n\n######### Spielregeln #########\n")
+print("Errate die Zahl zwischen 1 und 100\nDu hast genau 10 versuche um erfolgreich zu sein =)\n")
+input("Bestätige mit Enter um zu beginnen...")
+spiel_Start()
